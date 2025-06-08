0097851e126026556b473570157f8944ecd8a5b9
Richard Trygg Svartling <tryggrichard@gmail.com>
2025-06-08 14:57:24 +0200

    Sort/categories

--- Pages/Forums.cshtml.cs
+++ Pages/Forums.cshtml.cs
@@ -4,6 +4,7 @@ using No_Forum.Data;
 using No_Forum.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace No_Forum.Pages
 {
@@ -18,9 +19,47 @@ namespace No_Forum.Pages
 
         public List<Forumpages> ForumPages { get; set; } = new();
 
+        [BindProperty(SupportsGet = true)]
+        public List<string> Tags { get; set; } = new();
+
+        public List<string> SelectedTags => Tags ?? new List<string>();
+
         public void OnGet()
         {
-            ForumPages = _context.Forumpages.ToList();
+            var query = _context.Forumpages.AsQueryable();
+
+            if (Tags != null && Tags.Count > 0)
+            {
+                foreach (var tag in Tags)
+                {
+                    switch (tag)
+                    {
+                        case "Political":
+                            query = query.Where(f => f.Political);
+                            break;
+                        case "NSFW":
+                            query = query.Where(f => f.NSFW);
+                            break;
+                        case "Roleplay":
+                            query = query.Where(f => f.Roleplay);
+                            break;
+                        case "Discussion":
+                            query = query.Where(f => f.Discussion);
+                            break;
+                        case "Meme":
+                            query = query.Where(f => f.Meme);
+                            break;
+                        case "Art":
+                            query = query.Where(f => f.Art);
+                            break;
+                        case "Technology":
+                            query = query.Where(f => f.Technology);
+                            break;
+                    }
+                }
+            }
+
+            ForumPages = query.ToList();
         }
     }
 }
